3fd98060f1e7260ff4859738dd9cecbc2a037d92
timcondev <timconwaydev@outlook.com>
2024-01-30 13:50:54 -0500

    Convex setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,36 @@
-import { SignInButton, SignOutButton, auth } from '@clerk/nextjs';
+'use client';
+
+import { SignInButton, SignOutButton, useSession } from '@clerk/nextjs';
+import { useMutation, useQuery } from 'convex/react';
+import { api } from '../../convex/_generated/api';
 
 export default function Home() {
-  const { userId } = auth();
+  const { isSignedIn } = useSession();
+  const createThumbnail = useMutation(api.thumbnails.createThumbnail);
+  const thumbnails = useQuery(api.thumbnails.getThumbnailsForUser);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {userId ? <SignOutButton /> : <SignInButton />}
+    <main className="">
+      {isSignedIn ? <SignOutButton /> : <SignInButton />}
+      {isSignedIn && (
+        <form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const form = e.target as HTMLFormElement;
+            const formData = new FormData(form);
+            const title = formData.get('title') as string;
+            await createThumbnail({ title });
+            form.reset();
+          }}
+        >
+          <label>Title</label>
+          <input name="title" className="text-black"></input>
+          <button>Create</button>
+        </form>
+      )}
+      {thumbnails?.map((thumbnail) => {
+        return <div key={thumbnail._id}>{thumbnail.title}</div>;
+      })}
     </main>
   );
 }
